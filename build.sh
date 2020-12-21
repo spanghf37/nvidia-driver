25e051a40a59faf7877b52d892ff72d75f349c22
spanghf37 <spanghf37@gmail.com>
2020-12-21 08:20:17 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -xe
 
 # See the README file for a description of these variables
-BASE_URL='https://us.download.nvidia.com/tesla'
-#KERNEL_VERSION='4.18.0-193.13.2.el8_2.x86_64'
-DRIVER_VERSION='450.51.06'
-RHCOS_VERSION='4.5.6'
+# Tesla based card:
+# BASE_URL='https://us.download.nvidia.com/tesla'
+
+# Quadro card:
+BASE_URL='https://us.download.nvidia.com/XFree86/Linux-x86_64'
+
+KERNEL_VERSION='4.18.0-193.29.1.el8_2.x86_64'
+DRIVER_VERSION='450.80.02'
+RHCOS_VERSION='4.6.6'
 REGISTRY='quay.io'
-REPO='danclark'
+REPO='rhcsdel'
 
 KERNEL_VERSION=$(grep ${RHCOS_VERSION} README.md | awk -F\| '{print $3}' | tr -d ' ')
 
